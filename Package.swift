f018d0891065be69481cebb30928c2db9c628eeb
Nezhdanov Demian <nzhd.demian@gmail.com>
2023-04-10 02:48:00 -0300

    sadf

--- Package.swift
+++ Package.swift
@@ -19,8 +19,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+
         .binaryTarget(
             name: "MKDevice",
             path: "./Sources/MKDevice.xcframework")
