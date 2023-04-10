060798894ac6986eda90e44649c4ce3b18b2f284
Nezhdanov Demian <nzhd.demian@gmail.com>
2023-04-10 02:43:56 -0300

    sda

--- Package.swift
+++ Package.swift
@@ -6,27 +6,23 @@ import PackageDescription
 let package = Package(
     name: "MKDevice",
     platforms: [
-      .iOS(.v14)
+      .macOS(.v12), .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MKDevice",
-            targets: ["FrameworkName"]),
+            targets: ["MKDevice"]),
     ],
-   
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "MKDevice",
             path: "./Sources/MKDevice.xcframework")
-        
-    
-//        .target(
-//            name: <#String#>, resources: [
-//                 .process("Files/")
-//               ]
-//            )
     ]
 )
